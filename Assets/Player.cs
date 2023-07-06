9b74285c747aaa7b490088188e08d3e8d5b1c734
amamov <amamov@kakao.com>
2023-07-06 14:27:24 +0900

    feat : User Input

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody.AddForce(0, 1000, 0);
+        playerRigidbody = GetComponent<Rigidbody>(); // Generic
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log("Hello Unity");
+
+        // User Input
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity = velocity * speed;
+        velocity.y = fallSpeed;
+        playerRigidbody.velocity = velocity;
 
     }
 }
